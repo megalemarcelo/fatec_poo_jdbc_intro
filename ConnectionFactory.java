e422b2f108c643dd1af76408ed5e0abfec3d282a
Marcelo Megale <marcelo.megale@fatec.sp.gov.br>
2022-05-12 17:54:57 -0300

    Connection Factory criada

---
+++ ConnectionFactory.java
@@ -0,0 +1,23 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+public class ConnectionFactory {
+    private String host = "localhost";
+    private String port = "3306";
+    private String db = "20221_fatec_ipi_poo_pessoas";
+    private String user = "root";
+    private String password = "12345678";
+
+    public Connection getConnection() throws Exception {
+        return DriverManager.getConnection(
+            String.format(
+                "jdbc:mysql://%s:%s/%s?useTimezone=true&serverTimezone=UTC",
+                host,
+                port,
+                db
+            ),
+            user,
+            password
+        );
+    }
+}
